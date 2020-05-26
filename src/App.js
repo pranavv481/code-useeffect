41fba25e926b6a653ef003c8c468cecf06457775
pranavv481 <pranavv481@gmail.com>
2020-05-26 11:45:01 +0530

    hook effect

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import ClassCounterOne from './components/ClassCounterOne';
+import HookCounterOne from './components/HookCounterOne';
+
+
+class App extends Component {
+
+
+  render() {
+    return (
+      <div className="App">
+        <ClassCounterOne />
+        <HookCounterOne />
+
+      </div>
+    );
+  }
+}
+
+export default App;
